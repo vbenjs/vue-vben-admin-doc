74284ed976b60cc218a3a05189868517f505c5b2
踏学吾痕 <brucefxq@gmail.com>
2021-07-03 17:17:42 +0800

    docs: fix link (#28)

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -118,7 +118,7 @@ function createNav() {
         },
         {
           text: '文档源码',
-          link: 'https://github.com/anncwb/vue-vben-admin-doc',
+          link: 'https://github.com/anncwb/vben-admin-docs-cn',
         },
         {
           text: '更新日志',
